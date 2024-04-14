73eb94180b23bd88759f3632529f0d57432ba91b
FatmaBuyabes <buyabesfatemah@gmail.com>
2024-04-14 16:07:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Task 1
+//Q1
+int age = 23;
+double gpa = 3.41;
+string name = "Fatma Buyabes";
+bool student = true;
+char Letter = 'A';
+
+
+//Q2 
+double age2 = (double)age;
+int gpa2 = (int)gpa;
+Console.WriteLine($"{name} \nAge: {age}  \nGPA: {gpa} \nis a student: {student}");
+
+//Task 2
+//Q1
+var studentName = "Fatma";
+var studentAge = 23;
+var studentGpa = 3.41;
+var favSubject = "Web Design";
+
+Console.WriteLine($"student {name} \nGPA:{gpa} \nAge:{age} \nfavorite Subject:{favSubject}");
+
+//Q2
+const int length = 5;
+var width = 3.2;
+
+var area = length * width;
+Console.WriteLine(area);
+
+
